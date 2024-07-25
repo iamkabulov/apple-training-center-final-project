a899e971cde5343a01993aebece172a47bd7314b
kabulov <n.kabulov@cft.ru>
2024-07-25 15:51:26 +0500

    Cделал ButtonBuilder

--- musicPlayerApp/musicPlayerApp/View/ButtonBuilder.swift
+++ musicPlayerApp/musicPlayerApp/View/ButtonBuilder.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+class ButtonBuilder {
+	private let button: UIButton
+
+	init() {
+		self.button = UIButton()
+		self.button.translatesAutoresizingMaskIntoConstraints = false
+	}
+
+	func setImage(_ image: UIImage?, for state: UIControl.State) -> ButtonBuilder {
+		self.button.setImage(image, for: state)
+		return self
+	}
+
+	func setTitle(_ title: String?, for state: UIControl.State) -> ButtonBuilder {
+		self.button.setTitle(title, for: state)
+		return self
+	}
+
+	func setTitleColor(_ color: UIColor, for state: UIControl.State) -> ButtonBuilder {
+		self.button.setTitleColor(color, for: state)
+		return self
+	}
+
+	func setBackgroundColor(_ color: UIColor) -> ButtonBuilder {
+		self.button.backgroundColor = color
+		return self
+	}
+
+	func setTintColor(_ color: UIColor) -> ButtonBuilder {
+		self.button.tintColor = color
+		return self
+	}
+
+	func setTranslatesAutoresizingMaskIntoConstraints(_ flag: Bool) -> ButtonBuilder {
+		self.button.translatesAutoresizingMaskIntoConstraints = flag
+		return self
+	}
+
+	func addTarget(_ target: Any?, action: Selector, for event: UIControl.Event) -> ButtonBuilder {
+		self.button.addTarget(target, action: action, for: event)
+		return self
+	}
+
+	func build() -> UIButton {
+		return self.button
+	}
+}
